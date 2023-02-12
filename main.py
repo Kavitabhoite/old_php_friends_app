e0d5f816032989dbb3f8da2a6f303f7d50b91ca9
Kavitabhoite <kavita.bhoite11@gmail.com>
2023-02-12 19:10:03 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import phonenumbers
+from phonenumbers import geocoder
+ph1 = phonenumbers.parse("+919993862957")
+
+print("\nPhone Number's location :")
+print(geocoder.description_for_number(ph1,"en"))
+
+from phonenumbers import carrier
+print(carrier.name_for_number(ph1,"en"))
